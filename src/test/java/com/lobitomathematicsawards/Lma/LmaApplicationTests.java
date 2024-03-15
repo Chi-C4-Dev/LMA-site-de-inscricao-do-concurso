afcf42b5c66fe1ee50cf0f4dc0cf673475a19cd4
mesack chinhala <chinhalamesack@gmail.com>
2024-03-15 21:54:51 +0100

    alteração do Docker file

--- src/test/java/com/lobitomathematicsawards/Lma/LmaApplicationTests.java
+++ src/test/java/com/lobitomathematicsawards/Lma/LmaApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 class LmaApplicationTests {
 
 	@Test
+	
 	void contextLoads() {
 	}
 
